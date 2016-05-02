e7c785a2c38ad9088bfceaa9868d9fd978cf5181
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-05-02 11:30:54 -0700

    Adds a list to the main page.

--- Tropical_Fruits_App/app/src/main/java/com/example/randybiglow/tropical_fruits_app/MainActivity.java
+++ Tropical_Fruits_App/app/src/main/java/com/example/randybiglow/tropical_fruits_app/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.randybiglow.tropical_fruits_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Save space for the classes for this app.
+    LinkedList<String> mStringList;
+    ArrayAdapter<String> mAdapter;
+
+    //EditText mEditText;
+    //Button mButton;
+    //Above are stretch goals.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adds a list of tropical fruits.
+        //Once all of the requirements are done, I will add an option to add.
+        mStringList = new LinkedList<>();
+        mStringList.add("Jackfruit");
+        mStringList.add("Mangosteen");
+        mStringList.add("Cherimoya");
+        mStringList.add("Lychee");
+        mStringList.add("Pitaya");
+        mStringList.add("Waterapple");
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mStringList);
+
+        ListView listFruits = (ListView) (findViewById(R.id.mainListView));
+        listFruits.setAdapter((mAdapter));
+
+//        listFruits.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                //Intent intent = new Intent (MainActivity.this, FruitDetails.class);
+//            }
+//        });
     }
 }
