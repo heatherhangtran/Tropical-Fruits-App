485ba90cfbbfa1e0fff33e298190d415ff7f93e7
Randy Biglow <RandyBiglow@Randys-MacBook-Pro.local>
2016-05-04 11:37:45 -0700

    Adds a menu xml to res directory.

--- Tropical_Fruits_App/app/src/main/java/com/example/randybiglow/tropical_fruits_app/MainActivity.java
+++ Tropical_Fruits_App/app/src/main/java/com/example/randybiglow/tropical_fruits_app/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -14,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
     FruitDatabaseHelper dbHelper;
     SimpleCursorAdapter adapter;
     ListView listView;
-//    Toolbar toolbar;
+    Toolbar toolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        toolbar = (Toolbar)findViewById(R.id.mainToolbar);
-//        setSupportActionBar(toolbar);
+        toolbar = (Toolbar)findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
 
 
 
